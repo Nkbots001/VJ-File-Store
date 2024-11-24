69e7c0fe47b5460a07f63fb85852f683a493a5a4
Nkbots001 <nkstores@protonmail.com>
2024-11-24 20:43:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+import os
+import sys
+import socket
+
 from flask import Flask
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'TechVJ'
 
+@app.route("/")
+def hello_world():
+    version = sys.version_info
+    res = (
+        "<h1>Hi fellow devs on seenode</h1>"
+        f"<h2>{os.getenv('ENV')}</h2></br>"
+        f"Running Python: {version.major}.{version.minor}.{version.micro}<br>"
+        f"Hostname: {socket.gethostname()}"
+    )
+    return res
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(host="0.0.0.0", port=80)
